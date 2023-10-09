7c60c9ddbd7e0a7c70fd3bfb34b8b5df8d8eb732
DominykMcNamara <dominyksmith@gmail.com>
2023-10-08 21:18:29 -0400

    init

---
+++ app/Http/Controllers/SuggestionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FeedbackPosts;
+use Illuminate\Http\Request;
+
+class SuggestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SuggestionController $SuggestionController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SuggestionController $SuggestionController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SuggestionController $SuggestionController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SuggestionController $SuggestionController)
+    {
+        //
+    }
+}
