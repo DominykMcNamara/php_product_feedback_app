19b2041f1f0cd131d70eecb29b91638bbde8e642
DominykMcNamara <dominyksmith@gmail.com>
2023-10-12 22:02:40 -0400

    navbar ui edit

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -6,39 +6,40 @@ import UserAvatar from "./UserAvatar";
 export default function Navbar({ user }) {
     return (
         <div>
-            <div className="relative sm:flex sm:justify-center sm:items-center  bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-                <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right flex flex-row justify-around w-full">
-                    <div>
-                        <ApplicationLogo />
-                    </div>
-                    {user ? (
-                        <div>
-                            <Link
-                                href={route("dashboard")}
-                                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                            >
-                                Dashboard
-                            </Link>
-                            <UserAvatar user={user} />
-                        </div>
-                    ) : (
-                        <div className="flex flex-row">
-                            <Link
-                                href={route("login")}
-                                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                            >
-                                Log in
-                            </Link>
-
-                            <Link
-                                href={route("register")}
-                                className="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                            >
-                                Register
-                            </Link>
-                        </div>
-                    )}
+            <div
+                className="sm:fixed sm:top-0 sm:right-0
+                 text-right flex flex-row justify-around w-full py-2"
+            >
+                <div className="">
+                    <ApplicationLogo />
                 </div>
+                {user ? (
+                    <div className="flex flex-row w-[10rem] justify-between ">
+                        <Link
+                            href={route("dashboard")}
+                            className="font-semibold"
+                        >
+                            Dashboard
+                        </Link>
+                        <UserAvatar user={user} />
+                    </div>
+                ) : (
+                    <div className="flex flex-row">
+                        <Link
+                            href={route("login")}
+                            className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                        >
+                            Log in
+                        </Link>
+
+                        <Link
+                            href={route("register")}
+                            className="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                        >
+                            Register
+                        </Link>
+                    </div>
+                )}
             </div>
         </div>
     );
--- resources/js/Components/UserAvatar.jsx
+++ resources/js/Components/UserAvatar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function UserAvatar({ user}) {
   return (
     <div>
-        <img src={user.avatar} alt={`${user.name}'s avatar`} className="w-96 h-96 rounded-[100%]" />
+        <img src={user.avatar} alt={`${user.name}'s avatar`} className="w-[3rem] h-[3rem] rounded-[100%]" />
     </div>
   )
 }
