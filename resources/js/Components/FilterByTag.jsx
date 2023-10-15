4d92d779b94c3493841584ab132697c4770ce5bf
DominykMcNamara <dominyksmith@gmail.com>
2023-10-14 21:55:48 -0400

    Begin side panels

---
+++ resources/js/Components/FilterByTag.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FilterByTag() {
+  return (
+    <div className="w-[16rem] h-[10rem] bg-[#FFFFFF]">
+
+    </div>
+  )
+}
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,18 +1,26 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import FilterByTag from "@/Components/FilterByTag";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
 
 export default function Dashboard({ auth }) {
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-        >
+        <AuthenticatedLayout user={auth.user}>
             <Head title="Dashboard" />
+            <div className="flex flex-col w-[90rem] h-[83rem]">
+                <div className="bg-[url(https://file.rendit.io/n/nme2VAqJw3OcKiEAtHbh.svg)] bg-cover flex flex-col justify-end text-start h-[8.5rem] w-[20rem] rounded-xl">
+                    <div className="ml-[1.5rem] mb-[1.5rem]">
+                        <h1 className="text-xl  font-bold tracking-[-0.25] text-white">
+                            {" "}
+                            Frontend Mentor
+                        </h1>
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                        <p className="text-sm font-medium text-white">
+                            Feedback Board
+                        </p>
                     </div>
+                </div>
+                <div>
+                    <FilterByTag />
                 </div>
             </div>
         </AuthenticatedLayout>
